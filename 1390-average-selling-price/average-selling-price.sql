af43071c99c9027c0b94428d8e3eb0c281825666
Siddhant Chavan <chavansiddhant13@gmail.com>
2026-08-30 18:37:38 +0530

    Time: 844 ms (70.41%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -5,4 +5,4 @@ from Prices
 left join UnitsSold
 on Prices.product_id = UnitsSold.product_id
 and UnitsSold.purchase_date between Prices.start_date and Prices.end_date
-group by Prices.product_id
+group by product_id
